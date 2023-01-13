324d1a9035421bf7e6ebb8b3e0413dc5901172ee
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-01-13 21:09:49 +0600

    working!!

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@data': './src/data',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'nativewind/babel',
+  ],
 };
